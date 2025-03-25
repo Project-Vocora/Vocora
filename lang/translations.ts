de67cfb7765dee2e21442c397a29f2825225e509
tgarzagut <teresa.garzagut@gmail.com>
2025-03-25 01:26:55 -0500

    spanish settings (initial language toggle implementation)

---
+++ lang/translations.ts
@@ -0,0 +1,17 @@
+// translations.ts
+
+export const Settings_tr = {
+    en: {
+      title: "Settings",
+      options: "Settings Options:",
+      description: "⚙️ Settings.",
+      close: "Close",
+    },
+    es: {
+      title: "Configuraciones",
+      options: "Opciones de configuración:",
+      description: "⚙️ Configuraciones.",
+      close: "Cerrar",
+    },
+  };
+  
